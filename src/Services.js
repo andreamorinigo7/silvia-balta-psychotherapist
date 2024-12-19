78f7030ea182705db1daa8e8929f0ba1de8164fc
Andrea <morinigoandrea7@gmail.com>
2024-12-19 17:26:07 -0500

    service page started

--- src/Services.js
+++ src/Services.js
@@ -1,5 +1,43 @@
 import React from "react";
+import "./Services.css";
 
 export default function Services() {
-  return "hello from services";
+  return (
+    <div className="services">
+      <h2 className="mb-5">Background</h2>
+      <p className="m-0 background-details">
+        Director of the Ackerman Institute’s Social Work & Diversity Program
+        Silvia B. Espinal, LCSW is Director of the Latinx Youth and Family
+        Immigration Project, a clinic supervisor, and Director of the Ackerman
+        Institute’s Social Work & Diversity Program. Silvia began her family
+        therapy training while she was a graduate student at NYU Silver School
+        of Social Work and an intern in the Social Work and Diversity Program.
+        Her interest in family and couple therapy persisted, and she proceeded
+        to complete Ackerman’s post-graduate externship program.  In 2021,
+        Silvia joined Laurie Kaplan as Co-Director of the Social Work and
+        Diversity Program. Silvia is the first Social Work and Diversity Program
+        graduate to become a co-director, an honor she is committed to extend to
+        other graduates of the program.  In 2014, Silvia co-created the Ackerman
+        Institute’s Latinx Youth and Family Immigration Project. The project
+        offers therapy for Latinx families and community training to partner
+        agencies working with immigrant populations. As a Latina immigrant
+        herself, Silvia believes the Social Work and Diversity Program provided
+        a community and opportunities as a Latina immigrant to strengthen her
+        skills supporting BIPOC communities.  Silvia is currently an adjunct
+        faculty at the School of Social Work at Columbia University and Smith
+        College. Additionally, to her professional training at Ackerman, Silvia
+        completed a two-year group supervision training at the Eikenberg
+        Institute for Relationships in NYC where she focused on the integration
+        of diversity and issues of social justice with clinical cases.  As a
+        Latina immigrant and a family therapist, Silvia’s biculturalism and
+        bilingualism have helped her grow a genuine and unique passion in the
+        immigrant experience and she uses this strength-based approach when
+        working with these individuals, couples and families. In addition to
+        having a private practice in New York City, seeing a wide range of
+        diverse populations, Silvia has extensive experience working in
+        community mental health clinics, small psychotherapy group practices,
+        and large medical settings. 
+      </p>
+    </div>
+  );
 }
